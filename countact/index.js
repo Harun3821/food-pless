90193116ee47507be14721e425cad73a0337ba25
Harun3821 <harun4382183@gmail.com>
2024-01-18 17:17:02 +0600

    first commit food plesh

---
+++ countact/index.js
@@ -0,0 +1,14 @@
+
+var popp = document.getElementById("popp");
+   
+function openpopp(){
+  popp.classList.add("open-popp");
+}
+
+  
+function closepopp(){
+  popp.classList.remove("open-popp");
+}
+
+
+
